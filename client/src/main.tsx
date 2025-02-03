0cc45f149d2f6347386b55d85a47e56b373a7158
Rashmi <rr456rashmi@gmail.com>
2025-02-03 11:19:28 +0530

    default theme changed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const theme = extendTheme({
+  config: {
+      initialColorMode: "dark",
+  },
+});
+export default theme;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
@@ -16,7 +22,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
     <App />
     </ChakraProvider>
     </ClerkProvider>
